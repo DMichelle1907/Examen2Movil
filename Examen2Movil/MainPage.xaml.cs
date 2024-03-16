aab83444445353605e5835592933855b4cd8aca0
Daniela Reyes <reyesdaniela498@gmail.com>
2024-03-16 14:53:02 -0600

    Agregar archivos de proyecto.

---
+++ Examen2Movil/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace Examen2Movil
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void toolmenu_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolbarItem_Clicked_Nueva(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolbarItem_Clicked_salvadas(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvarUbicacion_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void btnListaUbicacion_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFoto_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGrabarAudio_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
---
+++ Examen2Movil/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Examen2Movil
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
